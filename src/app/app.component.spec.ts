5af32ec6a25356dae920a62066a75d27c25d8f20
VictorSordi <victorsordi200301@outlook.com>
2024-12-29 23:04:38 -0300

    Alterado pasta src, angular.json e server.ts

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -17,13 +17,13 @@ describe('AppComponent', () => {
   it(`should have the 'angular-blank' title`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('angular-blank');
+    expect(app.title).toEqual('angular-hello');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, angular-blank');
+    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, angular-hello');
   });
 });
